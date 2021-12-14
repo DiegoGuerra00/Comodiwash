da202256bade87df3176df349c7cfd87c40f35e3
DiegoGuerra00 <di.guerra00@gmail.com>
2021-12-13 22:37:25 -0300

    Progress on lights checklist and deleted some old place holders

--- lib/pages/checklist_pages/lights_checklist_page.dart.dart
+++ lib/pages/checklist_pages/lights_checklist_page.dart.dart
@@ -1,3 +1,4 @@
+import 'package:comodiwash/models/checklist_card.dart';
 import 'package:comodiwash/models/generic_app_bar.dart';
 import 'package:flutter/material.dart';
 
@@ -12,6 +13,21 @@ class _LightsChecklistPageState extends State<LightsChecklistPage> {
   double height = 85;
   final titleTextStyle = TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
   double circleRadiusExt = 13;
+  bool isFrontBanner =
+      true; // true if the banner with the front of the car is on the screen, false if the rear banner is beeing shown
+  int bannerIndex = 0;
+  String bannerPath = 'assets/images/ComodiWash_horizontal.png';
+
+  // Lights info from firestore
+  // TODO implement realtime database
+  bool lowBeamR = true;
+  bool lowBeamL = false;
+  bool highBeamR = true;
+  bool highBeamL = false;
+
+  // Text shown when the card is clicked
+  String lightsText =
+      'Cada veículo tem um tipo de lâmpada específico, podendo ser:\n-Halógena\n-Led\n-Xenon\n-Laser\nConsulte o manual do proprietário para checar o modelo correto';
 
   Widget buildBody() {
     return Column(
@@ -22,299 +38,164 @@ class _LightsChecklistPageState extends State<LightsChecklistPage> {
     );
   }
 
+  /// Change the banner image based on the boolean passed to the function
+  void changeBanner(int bannerIndex) {
+    switch (bannerIndex) {
+      case 0:
+        setState(() {
+          bannerPath = 'assets/images/ComodiWash_horizontal.png';
+        });
+        break;
+      case 1:
+        setState(() {
+          bannerPath = 'assets/images/user_profile_banner.png';
+        });
+        break;
+    }
+  }
+
   /// Build the banner at the top that shows in realtime all fault lights
   Widget carBanner() {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height * 0.3,
-      child: Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-        child: Center(
-          child: Container(
-            child: Image.asset('assets/images/ComodiWash_horizontal.png'),
-          ),
-        ),
-      ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [bannerBackButton(), bannerCard(), bannerForwardButton()],
     );
   }
 
+  Widget bannerCard() {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.7,
+      height: MediaQuery.of(context).size.height * 0.3,
+      child: Card(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          child: InkWell(
+            child: Center(
+                child: Container(
+              child: Image.asset(bannerPath),
+            )),
+          )),
+    );
+  }
+
+  Widget bannerBackButton() {
+    return ElevatedButton(
+      onPressed: () {
+        if (bannerIndex < 0) {
+          bannerIndex = 1;
+        } else {
+          bannerIndex--;
+        }
+
+        changeBanner(bannerIndex);
+      },
+      child: Container(
+          child: Icon(
+        Icons.arrow_back_ios,
+        size: 20,
+      )),
+      style: ElevatedButton.styleFrom(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
+          primary: Colors.grey,
+          minimumSize: Size(55, 50)),
+    );
+  }
+
+  Widget bannerForwardButton() {
+    return ElevatedButton(
+      onPressed: () {
+        if (bannerIndex > 1) {
+          bannerIndex = 0;
+        } else {
+          bannerIndex++;
+        }
+
+        changeBanner(bannerIndex);
+      },
+      child: Container(
+          child: Icon(
+        Icons.arrow_forward_ios,
+        size: 20,
+      )),
+      style: ElevatedButton.styleFrom(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
+          primary: Colors.grey,
+          minimumSize: Size(55, 50)),
+    );
+  }
+
+  /// Return column with the lighs cards
   Widget lightsCards() {
     return Column(
       children: [
-        lightLowBeamR(),
-        lightLowBeamL(),
-        lightHighBeamR(),
-        lightHighBeamL()
+        // Low beam right
+        ChecklistCard(
+            topText: 'Farol baixo',
+            bottonText: 'Direito',
+            statusDetailText: Text(''),
+            descriptionText: lightsText,
+            statusAvatarColor: getLowBeamRColor(lowBeamR)),
+        // Low beam left
+        ChecklistCard(
+            topText: 'Farol baixo',
+            bottonText: 'Esquerdo',
+            statusDetailText: Text(''),
+            descriptionText: lightsText,
+            statusAvatarColor: getLowBeamLColor(lowBeamL)),
+        // High beam right
+        ChecklistCard(
+            topText: 'Farol alto',
+            bottonText: 'Direito',
+            statusDetailText: Text(''),
+            descriptionText: lightsText,
+            statusAvatarColor: getHighBeamRColor(highBeamR)),
+        // High beam left
+        ChecklistCard(
+            topText: 'Farol alto',
+            bottonText: 'Esquerdo',
+            statusDetailText: Text(''),
+            descriptionText: lightsText,
+            statusAvatarColor: getHighBeamLColor(highBeamL))
       ],
     );
   }
 
-  /// Build the card of the right low beam
-  Widget lightLowBeamR() {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width,
-      height: height,
-      child: InkWell(
-        onTap: () {
-          showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  title: Text('Aviso'),
-                  content: Text(
-                      'Test'),
-                  actions: <Widget>[
-                    TextButton(
-                        onPressed: Navigator.of(context).pop,
-                        child: Text(
-                          'Ok',
-                          style:
-                              TextStyle(color: Color.fromRGBO(45, 26, 71, 1)),
-                        ))
-                  ],
-                );
-              });
-        },
-        child: Card(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Center(
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 10),
-                    child: Column(
-                      children: [
-                        Text('Farol baixo'),
-                        Text('Direito', style: titleTextStyle),
-                      ],
-                    ),
-                  ),
-                ),
-                Spacer(),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.03,
-                ),
-                CircleAvatar(
-                  radius: circleRadiusExt,
-                  backgroundColor: Colors.green,
-                ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.03,
-                ),
-                CircleAvatar(
-                  radius: circleRadiusExt,
-                  backgroundColor: Colors.red,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+  /// Gets the avatar color based on the state of the light from firestore
+  Color getLowBeamRColor(bool _lowBeamR) {
+    if (_lowBeamR) {
+      return Colors.green;
+    } else {
+      return Colors.red;
+    }
   }
 
-  /// Build the card of the left low beam
-  Widget lightLowBeamL() {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width,
-      height: height,
-      child: InkWell(
-        onTap: () {
-          showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  title: Text('Aviso'),
-                  content: Text(
-                      'Test'),
-                  actions: <Widget>[
-                    TextButton(
-                        onPressed: Navigator.of(context).pop,
-                        child: Text(
-                          'Ok',
-                          style:
-                              TextStyle(color: Color.fromRGBO(45, 26, 71, 1)),
-                        ))
-                  ],
-                );
-              });
-        },
-        child: Card(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Center(
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 10),
-                    child: Column(
-                      children: [
-                        Text('Farol baixo'),
-                        Text('Direito', style: titleTextStyle),
-                      ],
-                    ),
-                  ),
-                ),
-                Spacer(),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.03,
-                ),
-                CircleAvatar(
-                  radius: circleRadiusExt,
-                  backgroundColor: Colors.green,
-                ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.03,
-                ),
-                CircleAvatar(
-                  radius: circleRadiusExt,
-                  backgroundColor: Colors.red,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+  /// Gets the avatar color based on the state of the light from firestore
+  Color getLowBeamLColor(_lowbeamL) {
+    if (_lowbeamL) {
+      return Colors.green;
+    } else {
+      return Colors.red;
+    }
   }
 
-  /// Build the card of the right high beam
-  Widget lightHighBeamR() {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width,
-      height: height,
-      child: InkWell(
-        onTap: () {
-          showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  title: Text('Aviso'),
-                  content: Text(
-                      'Test'),
-                  actions: <Widget>[
-                    TextButton(
-                        onPressed: Navigator.of(context).pop,
-                        child: Text(
-                          'Ok',
-                          style:
-                              TextStyle(color: Color.fromRGBO(45, 26, 71, 1)),
-                        ))
-                  ],
-                );
-              });
-        },
-        child: Card(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Center(
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 10),
-                    child: Column(
-                      children: [
-                        Text('Farol baixo'),
-                        Text('Direito', style: titleTextStyle),
-                      ],
-                    ),
-                  ),
-                ),
-                Spacer(),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.03,
-                ),
-                CircleAvatar(
-                  radius: circleRadiusExt,
-                  backgroundColor: Colors.green,
-                ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.03,
-                ),
-                CircleAvatar(
-                  radius: circleRadiusExt,
-                  backgroundColor: Colors.red,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+  /// Gets the avatar color based on the state of the light from firestore
+  Color getHighBeamRColor(_highBeamR) {
+    if (_highBeamR) {
+      return Colors.green;
+    } else {
+      return Colors.red;
+    }
   }
 
-  /// Build the card of the left high beam
-  Widget lightHighBeamL() {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width,
-      height: height,
-      child: InkWell(
-        onTap: () {
-          showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  title: Text('Aviso'),
-                  content: Text(
-                      'Test'),
-                  actions: <Widget>[
-                    TextButton(
-                        onPressed: Navigator.of(context).pop,
-                        child: Text(
-                          'Ok',
-                          style:
-                              TextStyle(color: Color.fromRGBO(45, 26, 71, 1)),
-                        ))
-                  ],
-                );
-              });
-        },
-        child: Card(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Center(
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 10),
-                    child: Column(
-                      children: [
-                        Text('Farol baixo'),
-                        Text('Direito', style: titleTextStyle),
-                      ],
-                    ),
-                  ),
-                ),
-                Spacer(),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.03,
-                ),
-                CircleAvatar(
-                  radius: circleRadiusExt,
-                  backgroundColor: Colors.green,
-                ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.03,
-                ),
-                CircleAvatar(
-                  radius: circleRadiusExt,
-                  backgroundColor: Colors.red,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+  /// Gets the avatar color based on the state of the light from firestore
+  Color getHighBeamLColor(_highBeamL) {
+    if (_highBeamL) {
+      return Colors.green;
+    } else {
+      return Colors.red;
+    }
   }
 
   @override
