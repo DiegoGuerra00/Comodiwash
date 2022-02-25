559e15c4d25bdbae92074aa00c68722b0cf52170
DiegoGuerra00 <di.guerra00@gmail.com>
2022-02-25 20:11:35 -0300

    More tests on realtime database

--- lib/pages/test_pages/realtime_test.dart
+++ lib/pages/test_pages/realtime_test.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:comodiwash/databases/db_realtime.dart';
 import 'package:comodiwash/models/generic_app_bar.dart';
 import 'package:firebase_database/firebase_database.dart';
@@ -11,24 +13,81 @@ class RealtimeTest extends StatefulWidget {
 }
 
 class _RealtimeTestState extends State<RealtimeTest> {
-  // late FirebaseDatabase db = DBRealtime.get();
-  DatabaseReference ref = FirebaseDatabase.instance.ref("users/");
+  final database = FirebaseDatabase.instance.ref();
+  String _car = '';
+  String _name = '';
 
-  void testWrite() async {
-    await ref.set({'uid': 123, 'servico': 'polimento'});
+  String randomUID() {
+    List<String> numbers = [
+      '12434',
+      '6136',
+      '13567156',
+      '4251',
+      '1249576',
+      '1346756'
+    ];
+
+    return numbers[Random().nextInt(numbers.length)];
   }
 
-  void testRead() async {
-    ref.onValue.listen((DatabaseEvent event) {
-      final data = event.snapshot.value;
-      print(data);
+  String randomCar() {
+    List<String> cars = ['civic', 'gol', 'city', 'march', 'miata'];
+
+    return cars[Random().nextInt(cars.length)];
+  }
+
+  String randomName() {
+    List<String> names = [
+      'arthur',
+      'pedro',
+      'joana',
+      'ana',
+      'luan',
+      'fernanda'
+    ];
+
+    return names[Random().nextInt(names.length)];
+  }
+
+  void testWrite() async {
+    final userRef = database.child('users');
+    // final user = <String, dynamic>{'uid': 456, 'name': 'teste2'};
+
+    await userRef
+        .child(randomUID())
+        .set({'car': randomCar(), 'name': randomName()});
+    // await userRef.set({'uid': 456, 'nome': 'teste2'}).then((value) => print('Write sucess'));
+  }
+
+  void testRead() {
+    final userRef = database.child('users');
+    userRef
+        .child('4251')
+        .child('car')
+        .onValue
+        .listen((DatabaseEvent event) {
+      setState(() {
+        _car = event.snapshot.value as String;
+      });
+      print(event.snapshot.value);
+    });
+
+    userRef
+        .child('4251')
+        .child('name')
+        .onValue
+        .listen((DatabaseEvent event) {
+      setState(() {
+        _name = event.snapshot.value as String;
+      });
+      print(event.snapshot.value);
     });
   }
 
   void testUpdate() {
-    ref.update({
-      '123/uid': 1234
-    });
+    final userRef = database.child('users');
+
+    userRef.child('4251').update({'car': 'impala'});
   }
 
   @override
@@ -40,7 +99,11 @@ class _RealtimeTestState extends State<RealtimeTest> {
           children: [
             ElevatedButton(onPressed: () => testWrite(), child: Text('Write')),
             ElevatedButton(onPressed: () => testRead(), child: Text('Read')),
-            ElevatedButton(onPressed: () => testUpdate(), child: Text('Update')),
+            ElevatedButton(
+                onPressed: () => testUpdate(), child: Text('Update')),
+            Center(
+              child: Text('Nome: $_name Carro: $_car'),
+            )
           ],
         ),
       ),
